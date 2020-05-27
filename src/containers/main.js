a8d295b65ad4e0800f731dad24da5492a0fa8f91
Ali Hassan <alyhassan862@gmail.com>
2020-05-27 23:46:39 +0000

    data-api

--- src/containers/main.js
+++ src/containers/main.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import Headview from "./head.js";
 import "../overall.css"
 import Cont from "./contains.js";
-
+import {GET_DATA} from '../Data_APi'
 class Main extends Component {
     constructor()
     {
+        
         super()
         this.state = { 
             time:""
         }
+        GET_DATA();
+       
+
 
     }
+    componentDidMount(){
+            /*Fetching Data From Nasa Asteroids API's */
+        const data=[]
+        fetch('https://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=DEMO_KEY')
+        .then(response=>response.json())
+        .then(response=>data.push(response))
+        console.log(data)
+}
+    
     timeshow = () => {
 
         var x= new Date();
